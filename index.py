aa318bf1e3a5585dfd6e5b2a9ba3ba4416f3804b
Sajeesh V <sajeesh.v.tblr@gmai.com>
2023-08-23 08:59:48 +0530

    modifications

--- index.py
+++ index.py
@@ -6,9 +6,9 @@ def user_input(user_input_list_val,inputStr):
         default_list =user_input_list_val   
         display_patern(default_list)
         if is_all_index_filled(default_list)==False:
-            row =str(input(f'{Fore.BLUE}please enter a row number from 1,2,3 to insert {Fore.RED}{inputStr}{Fore.RESET}\n'))
+            row =input(f'{Fore.BLUE}Please choose a ROW from 1,2,3 to insert {Fore.RED}{inputStr}!!{Fore.RESET}\n')
             if validate(row):
-                col = str(input(f'{Fore.BLUE}please enter a column number from 1,2,3 to insert {Fore.RED}{inputStr}{Fore.RESET}\n'))
+                col = input(f'{Fore.BLUE}Please choose a COLUMN from 1,2,3 to insert {Fore.RED}{inputStr}!!{Fore.RESET}\n')
                 if validate(col):
                     if check_value_exist_in_index(default_list,int(row)-1,int(col)-1):
                         default_list[int(row)-1][int(col)-1]=inputStr
@@ -19,20 +19,16 @@ def user_input(user_input_list_val,inputStr):
                             inputStr='X' 
                         find_game_winner(default_list,inputStr)       
                     else:
-                        print(f"{Fore.RED}Error::user already added value in this cell{Fore.RESET}")
-                        user_input(default_list,inputStr)    
+                        display_error(f"{Fore.RED}Sorry!!. Already  added value in this box (row-{int(row)},column-{int(col)})!{Fore.RESET}",default_list,inputStr)    
                 else:
-                    print(f"{Fore.RED}Error::invalid input*{Fore.RESET}")
-                    user_input(default_list,inputStr)    
+                    display_error(f"{Fore.RED}invalid selection!{Fore.RESET}",default_list,inputStr)    
             else:
-                print(f"{Fore.RED}Error::invalid input*{Fore.RESET}")
-                user_input(default_list,inputStr)
+                display_error(f"{Fore.RED}invalid selection!{Fore.RESET}",default_list,inputStr)
         else:
             user_input(user_input_list,inputStr)
 
     except Exception as e:
-        print(f"{Fore.RED}Error::invalid input*{Fore.RESET}")
-        user_input(user_input_list,"X")
+        display_error(f"{Fore.RED}invalid selection!{Fore.RESET}",user_input_list,"X")
 
 def display_patern(default_list):
     print(" | ".join(default_list[0]))
@@ -46,7 +42,7 @@ def is_all_index_filled(default_list):
     return merge_list.count("X")+merge_list.count('O')==9
 
 def validate(value):
-    if not value in ['1','2','3']:
+    if not value.isdigit():
         return False
     x=range(0,3)
     return int(value)-1 in x
@@ -78,19 +74,25 @@ def find_game_winner(default_list,inputStr="X"):
     
     if winner:
         display_patern(default_list)
-        print(f'{Fore.GREEN}{winner} wins the game..Game Over{Fore.RESET}')
-        print("Restarting game...")
+        display_message(f'{Fore.GREEN}{winner} wins the game..Game Over!!{Fore.RESET}')
+        display_message("Restarting game...!!")
         user_input(dummyList,"X")
         return
     elif is_all_index_filled(default_list) and not winner:
         display_patern(default_list)
-        print(f'{Fore.YELLOW}Game Drawn{Fore.RESET}')
-        print("Restarting game...")
+        display_message(f'{Fore.YELLOW}Game Drawn!!{Fore.RESET}')
+        display_message("Restarting game...!!")
         user_input(dummyList,"X")
         return   
     else:
         user_input(default_list,inputStr)
-        return 
+        return
+     
+def display_error(message,default_list,inputStr):
+    print(message)
+    user_input(default_list,inputStr)
 
+def display_message(message):
+    print(message)
 user_input(user_input_list,inputStr)
     
