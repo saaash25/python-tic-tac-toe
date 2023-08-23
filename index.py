9d3bc7e8748edcd11201a0af64438a716c2116d6
Sajeesh V <sajeesh.v.tblr@gmai.com>
2023-08-23 10:59:57 +0530

    modifications

--- index.py
+++ index.py
@@ -1,72 +1,73 @@
 from colorama import Fore
-user_input_list =[[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+import re
+import time
+user_input_list =['{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}']
 inputStr ='X'
 def user_input(user_input_list_val,inputStr):
     try: 
         default_list =user_input_list_val   
         display_patern(default_list)
         if is_all_index_filled(default_list)==False:
-            row =input(f'{Fore.BLUE}Please choose a ROW from 1,2,3 to insert {Fore.RED}{inputStr}!!{Fore.RESET}\n')
-            if validate(row):
-                col = input(f'{Fore.BLUE}Please choose a COLUMN from 1,2,3 to insert {Fore.RED}{inputStr}!!{Fore.RESET}\n')
-                if validate(col):
-                    if check_value_exist_in_index(default_list,int(row)-1,int(col)-1):
-                        default_list[int(row)-1][int(col)-1]=inputStr
-                        tempInput =inputStr
-                        if tempInput=='X':
-                            inputStr='O'
-                        else:
-                            inputStr='X' 
-                        find_game_winner(default_list,inputStr)       
+            position =input(f'{Fore.BLUE}Enter a position from 1-9 to insert {Fore.RED}{inputStr}!!{Fore.RESET}\n')
+            if validate(position):
+                if check_value_exist_in_index(default_list,int(position)-1):
+                    default_list[int(position)-1]=f' {inputStr} '
+                    tempInput =inputStr
+                    if tempInput=='X':
+                        inputStr='O'
                     else:
-                        display_error(f"{Fore.RED}Sorry!!. Already  added value in this box (row-{int(row)},column-{int(col)})!{Fore.RESET}",default_list,inputStr)    
+                        inputStr='X' 
+                    find_game_winner(default_list,inputStr)       
                 else:
-                    display_error(f"{Fore.RED}invalid selection!{Fore.RESET}",default_list,inputStr)    
+                    display_error(f"{Fore.RED}Sorry!!. Already  added value in this box (position-{int(position)})!{Fore.RESET}",default_list,inputStr)    
             else:
                 display_error(f"{Fore.RED}invalid selection!{Fore.RESET}",default_list,inputStr)
         else:
             user_input(user_input_list,inputStr)
 
     except Exception as e:
-        display_error(f"{Fore.RED}invalid selection!{Fore.RESET}",user_input_list,"X")
+        print(e)
+        display_error(f"{Fore.RED}something went wrong!!{Fore.RESET}",user_input_list,"X")
+        do_start_game()
 
 def display_patern(default_list):
-    print(" | ".join(default_list[0]))
-    print("--|---|--")
-    print(" | ".join(default_list[1]))
-    print("--|---|--")
-    print(" | ".join(default_list[2]))
+    print("   |   | ")
+    print("|".join(default_list[0:3]))
+    print("   |   | ")
+    print("---|---|---")
+    print("|".join(default_list[3:6]))
+    print("   |   | ")
+    print("---|---|---")
+    print("   |   | ")
+    print("|".join(default_list[6::]))
+    print("   |   | ")
 
 def is_all_index_filled(default_list):
-    merge_list = default_list[0]+default_list[1]+default_list[2]
-    return merge_list.count("X")+merge_list.count('O')==9
+    return default_list.count(" X ")+default_list.count(' O ')==9
 
 def validate(value):
     if not value.isdigit():
         return False
-    x=range(0,3)
+    x=range(0,9)
     return int(value)-1 in x
 
-def check_value_exist_in_index(default_list,row,col):
-    return default_list[row][col] == " "
+def check_value_exist_in_index(default_list,position):
+    x = re.search(r"{[1-9]}",default_list[position])
+    return x != None
 
 def find_game_winner(default_list,inputStr="X"):
     winner=""
-    dummyList=[[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
-    possibleWinEntries=["".join([default_list[0][0],default_list[0][1],default_list[0][2]]),
-                        "".join([default_list[1][0],default_list[1][1],default_list[1][2]]),
-                        "".join([default_list[2][0],default_list[2][1],default_list[2][2]]),
-                        "".join([default_list[0][0],default_list[1][0],default_list[2][0]]),
-                        "".join([default_list[0][1],default_list[1][1],default_list[2][1]]),
-                        "".join([default_list[0][2],default_list[1][2],default_list[2][2]]),
-                        "".join([default_list[0][0],default_list[1][1],default_list[2][2]]),
-                        "".join([default_list[0][2],default_list[1][1],default_list[2][0]])
+    possibleWinEntries=[default_list[0].strip()+default_list[1].strip()+default_list[2].strip(),
+                        default_list[3].strip()+default_list[4].strip()+default_list[5].strip(),
+                        default_list[6].strip()+default_list[7].strip()+default_list[8].strip(),
+                        default_list[0].strip()+default_list[3].strip()+default_list[6].strip(),
+                        default_list[1].strip()+default_list[4].strip()+default_list[7].strip(),
+                        default_list[2].strip()+default_list[5].strip()+default_list[8].strip(),
+                        default_list[0].strip()+default_list[4].strip()+default_list[8].strip(),
+                        default_list[2].strip()+default_list[4].strip()+default_list[6].strip()
                         ]
     for item in possibleWinEntries:
-        if item=="XXX":
-            winner=item[0]
-            break
-        elif item=="OOO":
+        if item=="XXX" or item=="OOO":
             winner=item[0]
             break
         else:
@@ -75,14 +76,12 @@ def find_game_winner(default_list,inputStr="X"):
     if winner:
         display_patern(default_list)
         display_message(f'{Fore.GREEN}{winner} wins the game..Game Over!!{Fore.RESET}')
-        display_message("Restarting game...!!")
-        user_input(dummyList,"X")
+        do_start_game()
         return
     elif is_all_index_filled(default_list) and not winner:
         display_patern(default_list)
         display_message(f'{Fore.YELLOW}Game Drawn!!{Fore.RESET}')
-        display_message("Restarting game...!!")
-        user_input(dummyList,"X")
+        do_start_game()
         return   
     else:
         user_input(default_list,inputStr)
@@ -94,5 +93,29 @@ def display_error(message,default_list,inputStr):
 
 def display_message(message):
     print(message)
-user_input(user_input_list,inputStr)
-    
+
+def display_initial_messages(initial_list,initialValue):
+    print("Starting game...!!")
+    while True:
+        time.sleep(1)
+        print("-----------------------------")
+        print("1. 'X' for player 1 and 'O' for player 2")
+        print("2. There will be boxes 1-9. each user choose 1 from this to inser their value")
+        print("3. Cant't insert values if already a value is there in a box")
+        print("------------------------------")
+        user_input(initial_list,initialValue)
+
+def do_start_game():
+    start="WRONG"
+    while start.lower()!="y" or start.lower()!="n":
+        start=input("Want to play again?\nEnter Y for Yes OR N for No\n")
+        if start.lower()=="y":
+            display_initial_messages(['{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}'],'X')
+        elif start.lower()=="n":
+            exit()
+        else:
+            print(f'{Fore.RED}Accept only Y or N{Fore.RESET}')    
+
+display_initial_messages(user_input_list,inputStr)
+
+     
